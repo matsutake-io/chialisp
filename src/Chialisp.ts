9452bfa633d1175067a69ca188ff709cd17b4f64
Taro <taro1999@pm.me>
2021-10-17 17:43:40 +0100

    browser support

--- src/Chialisp.ts
+++ src/Chialisp.ts
@@ -1,19 +1,27 @@
 import { go, setPrintFunction } from 'clvm_tools';
+import { go as browserGo, setPrintFunction as browserSetPrintFunction } from 'clvm_tools/browser';
 import { Clvm } from './Clvm';
 import { Program } from './Program';
 
 export class Chialisp implements Program {
-    constructor(private readonly chialisp: string) {}
+    constructor(private readonly chialisp: string, private readonly browser = true) {}
 
     public async hex(): Promise<string> {
-        const clvm = await this.clvm();
+        const clvm = await this.clvm(this.browser);
 
         return clvm.hex();
     }
 
-    public async clvm(): Promise<Clvm> {
+    public async clvm(browser: boolean = true): Promise<Clvm> {
         return new Promise(resolve => {
-            setPrintFunction(clvm => resolve(new Clvm(clvm)));
+            if (this.browser) {
+                browserSetPrintFunction(clvm => resolve(new Clvm(clvm, browser)));
+                browserGo('run', this.chialisp);
+
+                return;
+            }
+
+            setPrintFunction(clvm => resolve(new Clvm(clvm, browser)));
             go('run', this.chialisp);
         });
     }
--- src/Clvm.ts
+++ src/Clvm.ts
@@ -1,11 +1,19 @@
 import { go, setPrintFunction } from 'clvm_tools';
+import { go as browserGo, setPrintFunction as browserSetPrintFunction } from 'clvm_tools/browser';
 import { Program } from './Program';
 
 export class Clvm implements Program {
-    public constructor(private readonly clvm: string) {}
+    public constructor(private readonly clvm: string, private readonly browser = true) {}
 
     public async hex(): Promise<string> {
         return new Promise(resolve => {
+            if (this.browser) {
+                browserSetPrintFunction(hex => resolve(hex));
+                browserGo('opc', '-H', this.clvm);
+
+                return;
+            }
+
             setPrintFunction(hex => resolve(hex));
             go('opc', '-H', this.clvm);
         });
